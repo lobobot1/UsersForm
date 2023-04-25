1a88cd52aa28bbae8eab75652c42968de71239cb
Reynaldo Gonzalez <62901498+kurokuro15@users.noreply.github.com>
2023-04-25 20:21:23 +0000

    Add GET/POST /api/forms & /api/forms/create EP

---
+++ app/api/forms/create/route.js
@@ -0,0 +1,58 @@
+import isAdminRequest, { getCookieId } from '@lib/auth/isAdminRequest'
+import {
+  someFieldMissing,
+  somePrismaError,
+  unauthorized,
+} from '@lib/http/ErrorHandler'
+import { creationResponse } from '@lib/http/ResponseHandler'
+import prisma from '@lib/prisma'
+import { NextRequest, NextResponse } from 'next/server'
+
+/**
+ *
+ * @param { NextRequest } request
+ * @returns { NextResponse }
+ */
+export async function POST(request) {
+  if (!(await isAdminRequest(request)))
+    return unauthorized({ entity: 'create forms' })
+
+  const id = getCookieId(request)
+  const body = await request.json()
+
+  if (!validateBody(body)) return someFieldMissing()
+  const { revisionText, questions } = body
+  try {
+    const data = await prisma.form.create({
+      data: {
+        authorId: id,
+        revisionText,
+        question: {
+          connectOrCreate: questions.map((question) => ({
+            create: { question },
+            where: { question },
+          })),
+        },
+      },
+      select: {
+        id: true,
+      },
+    })
+    if (!data) return fatality()
+
+    return creationResponse({ entity: 'form', id: data.id })
+  } catch (error) {
+    return somePrismaError(error)
+  }
+}
+
+/**
+ * check body
+ * @param { any } body
+ * @returns { boolean }
+ */
+function validateBody(body) {
+  const { revisionText, questions } = body
+
+  return Boolean(revisionText || questions || questions.length > 0)
+}
---
+++ app/api/forms/route.js
@@ -0,0 +1,68 @@
+import isAdminRequest from '@lib/auth/isAdminRequest'
+import { isLoggedRequest } from '@lib/auth/isLoggedRequest'
+import { fatality } from '@lib/http/ErrorHandler'
+import { successListResponse } from '@lib/http/ResponseHandler'
+import prisma from '@lib/prisma'
+import { NextRequest } from 'next/server'
+
+/**
+ * @param { NextRequest} request
+ */
+export async function GET(request) {
+  if (!isLoggedRequest()) return unauthorized({ entity: 'read forms' })
+  const isAdmin = await isAdminRequest(request)
+
+  const data = await prisma.form.findMany({
+    select: {
+      id: true,
+      author: {
+        select: {
+          id: true,
+          email: true,
+          name: true,
+          lastname: true,
+        },
+      },
+      createdAt: true,
+      question: {
+        select: {
+          id: true,
+          question: true,
+          PossibleAnswer: {
+            select: {
+              id: true,
+              answer: true,
+            },
+          },
+          _count: true,
+        },
+      },
+      revisionText: true,
+      FormAnswered: {
+        select: {
+          id: true,
+          answer: true,
+          status: {
+            select: {
+              id: true,
+              status: true,
+            },
+          },
+          user: {
+            select: {
+              id: true,
+              email: true,
+              name: true,
+              lastname: true,
+            },
+          },
+        },
+      },
+    },
+    orderBy: { updatedAt: 'desc' },
+  })
+
+  if (!data) return fatality()
+
+  return successListResponse({ data })
+}
