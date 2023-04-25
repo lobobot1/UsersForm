b3a27d499d6bd9a0ea0055eef3ec8181161d01a4
Yhan Montaño <67387450+XlichOpX@users.noreply.github.com>
2023-04-25 13:40:18 +0000

    Add JSDoc to Input_lable component

--- app/components/Input_label.jsx
+++ app/components/Input_label.jsx
@@ -1,23 +1,28 @@
-import { forwardRef } from "react";
+import { forwardRef } from 'react'
 
-const Input_label = forwardRef(({ label, id, type = "text", ...props }, ref) => {
-  return (
-    <div className="flex flex-col">
-      <label htmlFor={id} className="mb-2 text-lg font-normal">
-        {label}
-      </label>
+/** @typedef {{ label: string } & import('react').ComponentPropsWithRef<'input'>} InputProps */
 
-      <input
-        ref={ref}
-        type={type}
-        id={id}
-        className="focus:outline-none focus:border-cyan-500 py-1 pl-2 rounded-md border-2 border-gray-300 w-full"
-        {...props}
-      />
-    </div>
-  );
-});
+/** @type {import('react').ForwardRefRenderFunction<HTMLInputElement, InputProps>} */
+const Input_label = forwardRef(
+  ({ label, id, type = 'text', ...props }, ref) => {
+    return (
+      <div className='flex flex-col'>
+        <label htmlFor={id} className='mb-2 text-lg font-normal'>
+          {label}
+        </label>
 
-Input_label.displayName = "Input_label";
+        <input
+          ref={ref}
+          type={type}
+          id={id}
+          className='focus:outline-none focus:border-cyan-500 py-1 pl-2 rounded-md border-2 border-gray-300 w-full'
+          {...props}
+        />
+      </div>
+    )
+  }
+)
 
-export default Input_label;
+Input_label.displayName = 'Input_label'
+
+export default Input_label
