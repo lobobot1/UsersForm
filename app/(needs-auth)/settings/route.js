9899816b2a6caf8b52c0f905dbe4ca3bc9db7a77
Yhan Montaño <67387450+XlichOpX@users.noreply.github.com>
2023-04-25 17:09:14 +0000

    Redirect /settings to /settings/questions

--- app/(needs-auth)/settings/route.js
+++ app/(needs-auth)/settings/route.js
@@ -1,6 +1,8 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server'
 
 /** @param {import("next/server").NextResponse} req */
 export const GET = (req) => {
-  return NextResponse.redirect(new URL("/settings/answers", process.env.BASE_URL || req.url));
-};
+  return NextResponse.redirect(
+    new URL('/settings/questions', process.env.BASE_URL || req.url)
+  )
+}
