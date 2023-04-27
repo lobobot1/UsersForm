5bc565cd65d1bb7f636f41d9a7ff5dd9f08fa723
Yhan Montaño <67387450+XlichOpX@users.noreply.github.com>
2023-04-27 18:59:43 +0000

    Add logout link to NavbarUser

--- app/(needs-auth)/NavbarUser.jsx
+++ app/(needs-auth)/NavbarUser.jsx
@@ -1,13 +1,29 @@
+import Link from 'next/link'
 
 const NavbarUser = () => {
   return (
     <nav className='bg-cyan-500 h-12 flex justify-between items-center px-10'>
       <h3>User</h3>
-      <button className=" text-red-700 hover:scale">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M5.636 5.636a9 9 0 1012.728 0M12 3v9" />
+      <Link
+        href='/api/auth/logout'
+        className='text-red-700 hover:scale'
+        prefetch={false}
+      >
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          stroke-width='1.5'
+          stroke='currentColor'
+          class='w-6 h-6'
+        >
+          <path
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            d='M5.636 5.636a9 9 0 1012.728 0M12 3v9'
+          />
         </svg>
-      </button>
+      </Link>
     </nav>
   )
 }
