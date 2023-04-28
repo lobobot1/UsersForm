47de8bb4a334139d147fd957fa855e05e84d13e2
Yhan Montaño <67387450+XlichOpX@users.noreply.github.com>
2023-04-28 13:43:30 +0000

    Add spinner to CreateFormForm

--- app/(admin-pages)/settings/forms/CreateFormForm.jsx
+++ app/(admin-pages)/settings/forms/CreateFormForm.jsx
@@ -1,32 +1,40 @@
 'use client'
-import useForms from '@/app/hooks/useForms'
-import FormForm from './FormForm'
 import ErrorAlert from '@/app/components/ErrorAlert'
-import { useState } from 'react'
+import Spinner from '@/app/components/Spinner'
+import useForms from '@/app/hooks/useForms'
 import useQuestions from '@/app/hooks/useQuestions'
+import { useState } from 'react'
+import FormForm from './FormForm'
 
 const CreateFormForm = () => {
   const [error, setError] = useState(null)
   const { createForm } = useForms()
-  const { questions } = useQuestions()
+  const { questions, isLoading } = useQuestions()
 
   return (
     <>
       {error && <ErrorAlert>{error}</ErrorAlert>}
-      {questions && <FormForm
-        resetOnSuccess
-        onSubmit={async (data) => {
-          try {
-            setError(null)
-            await createForm(data)
-          } catch (err) {
-            if (err instanceof Error) {
-              setError(err.message)
+      {isLoading && (
+        <div className='flex items-center justify-center'>
+          <Spinner />
+        </div>
+      )}
+      {questions && (
+        <FormForm
+          resetOnSuccess
+          onSubmit={async (data) => {
+            try {
+              setError(null)
+              await createForm(data)
+            } catch (err) {
+              if (err instanceof Error) {
+                setError(err.message)
+              }
             }
-          }
-        }}
-        questions={questions.data}
-      />}
+          }}
+          questions={questions.data}
+        />
+      )}
     </>
   )
 }
