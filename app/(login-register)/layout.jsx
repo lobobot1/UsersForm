7379c49898c072d69f9caa6a592d2dd573eefeeb
Yhan Montaño <67387450+XlichOpX@users.noreply.github.com>
2023-04-28 12:20:05 +0000

    Redirect to an existent route after login

--- app/(login-register)/layout.jsx
+++ app/(login-register)/layout.jsx
@@ -1,16 +1,16 @@
-import { isLoggedRequest } from '@lib/auth/isLoggedRequest';
-import { redirect } from "next/navigation";
+import { isLoggedRequest } from '@lib/auth/isLoggedRequest'
+import { redirect } from 'next/navigation'
 
 export default function RootLayout({ children }) {
   const isLogged = isLoggedRequest()
 
   if (isLogged) {
-    redirect("/form")
+    redirect('/user')
   }
 
   return (
-    <div className="min-w-screen min-h-screen bg-gray-800 flex flex-col items-center justify-center p-5">
+    <div className='min-w-screen min-h-screen bg-gray-800 flex flex-col items-center justify-center p-5'>
       {children}
     </div>
-  );
+  )
 }
