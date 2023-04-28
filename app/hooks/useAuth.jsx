32c683e5f442d6098eddd4c9aac85813d6571961
Yhan Montaño <67387450+XlichOpX@users.noreply.github.com>
2023-04-28 14:42:12 +0000

    Delete unused code

--- app/hooks/useAuth.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import AuthContext from "../context/AuthContext";
-
-// eslint-disable-next-line react-hooks/rules-of-hooks
-function useAuth() {
-  return useContext(AuthContext);
-}
-
-export default useAuth;
