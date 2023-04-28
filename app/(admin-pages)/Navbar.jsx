79003f95b164431017f5e9ecede2bcdd3a4ac554
Yhan Montaño <67387450+XlichOpX@users.noreply.github.com>
2023-04-28 14:02:29 +0000

    Add iconst o admin navbar

--- app/(admin-pages)/Navbar.jsx
+++ app/(admin-pages)/Navbar.jsx
@@ -1,17 +1,23 @@
 import Link from 'next/link'
+import Gear from '../components/icons/Gear'
+import Logout from '../components/icons/Logout'
 
 const links = [
-  { href: '/form', label: 'Forms' },
-  { href: '/users', label: 'Users' },
-  { href: '/settings/questions', label: 'Settings' },
-  { href: '/api/auth/logout', label: 'Logout', dontPrefetch: true },
+  { href: '/settings/questions', label: 'Settings', icon: <Gear /> },
+  {
+    href: '/api/auth/logout',
+    label: 'Logout',
+    className: 'text-red-500',
+    icon: <Logout />,
+    dontPrefetch: true,
+  },
 ]
 
 const Navbar = () => {
   return (
     <header className='bg-white'>
       <div className='container mx-auto p-2 flex items-center justify-between'>
-        <div>UserForms</div>
+        <div className='font-bold text-lg'>Admin dashboard</div>
 
         <nav>
           <ul className='flex gap-5'>
@@ -20,7 +26,9 @@ const Navbar = () => {
                 <Link
                   href={link.href}
                   prefetch={link.dontPrefetch ? false : undefined}
+                  className={'flex items-center gap-2' + ` ${link.className}`}
                 >
+                  {link.icon}
                   {link.label}
                 </Link>
               </li>
