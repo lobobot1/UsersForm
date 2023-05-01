2e846772c441da77e15a50bd4a6743a22f49b68d
Yhan Montaño <yhan.carlos2001@gmail.com>
2023-05-01 16:38:50 -0400

    Redirect homepage to /user

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,7 @@
-'use client'
-import Modal from '@/app/components/Modal'
-import { useState } from 'react'
+import { redirect } from 'next/navigation'
 
 const Page = () => {
-  const [isOpen, setIsOpen] = useState(false)
-
-  return (
-    <div>
-      <Modal
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-        title={<h2>Hola modal</h2>}
-        body={<p>Hola modal body</p>}
-      />
-      <button className='bg-white p-2' onClick={() => setIsOpen(true)}>
-        Abrir modal
-      </button>
-    </div>
-  )
+  return redirect('/user')
 }
 
 export default Page
