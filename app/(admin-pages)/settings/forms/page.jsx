f8eb67886e4ad69b72cfbe6a87053fe900de3e95
Yhan <yhan.carlos2001@gmail.com>
2023-05-04 13:09:32 -0400

    Add DownloadSpreadsheetButton

--- app/(admin-pages)/settings/forms/page.jsx
+++ app/(admin-pages)/settings/forms/page.jsx
@@ -1,4 +1,5 @@
 import CreateFormForm from './CreateFormForm'
+import DownloadSpreadsheetButton from './DownloadSpreadsheetButton'
 import FormList from './FormList'
 
 export const metadata = {
@@ -7,17 +8,20 @@ export const metadata = {
 
 const Page = async () => {
   return (
-    <div className='lg:flex gap-8'>
+    <div className='gap-8 lg:flex'>
       <div className='grow'>
-        <h1 className='text-xl font-bold mb-3'>Forms</h1>
+        <div className='flex items-center justify-between mb-3'>
+          <h1 className='text-xl font-bold'>Forms</h1>
+          <DownloadSpreadsheetButton />
+        </div>
         <FormList />
       </div>
 
       <div className='w-full lg:max-w-sm shrink-0'>
-        <div className='mt-3 lg:mt-0 mb-3'>
+        <div className='mt-3 mb-3 lg:mt-0'>
           <h2 className='text-xl font-bold lg:mt-0'>Create a new form</h2>
         </div>
-        <div className='bg-white p-2 text-black rounded-md'>
+        <div className='p-2 text-black bg-white rounded-md'>
           <CreateFormForm />
         </div>
       </div>
